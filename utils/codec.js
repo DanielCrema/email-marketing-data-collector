3ba51c452224c9d33fd5537e432a6477b8d3f5f1
DanielCrema <danielborgescrema@gmail.com>
2024-06-04 19:56:21 -0300

    debug codec constructor

--- utils/codec.js
+++ utils/codec.js
@@ -6,17 +6,25 @@ class codec {
             'f': '7', 'g': 'X', 'h': '3', 'i': 'Z', 'j': 'y',
             'k': 'g', 'l': 'j', 'm': '4', 'n': '6', 'o': 'C',
             'p': 'T', 'q': 'k', 'r': 'S', 's': 'm', 'u': 'L',
-            't': 'b', 'u': 'N', 'v': 'M', 'w': 'd', 'x': 'F',
-            'y': 'I', 'z': 'A', '0': 's', '1': '2', '2': '9',
-            '3': 'h', '4': 'p', '5': 'x', '6': '0', '7': 'U',
-            '8': 'G', '9': 'H', '-': 'i', '_': 'l', '@': this.randomAtValue(), '.': this.randomDotValue()
+            't': 'b', 'v': 'M', 'w': 'd', 'x': 'F', 'y': 'I',
+            'z': 'A', '0': 's', '1': '2', '2': '9', '3': 'h',
+            '4': 'p', '5': 'x', '6': '0', '7': 'U', '8': 'G',
+            '9': 'H', '-': 'i', '_': 'l', '@': this.randomAtValue(), '.': this.randomDotValue()
         };
 
         // Reverse charMap to create decoding map
-        this.decodeMap = {};
-        for (let key in this.charMap) {
-            this.decodeMap[this.charMap[key]] = key;
-        }
+        this.decodeMap = {
+            'f': 'a', 'q': 'b', 'e': 'c', 'w': 'd', '1': 'e',
+            '7': 'f', 'X': 'g', '3': 'h', 'Z': 'i', 'y': 'j',
+            'g': 'k', 'j': 'l', '4': 'm', '6': 'n', 'C': 'o',
+            'T': 'p', 'k': 'q', 'S': 'r', 'm': 's', 'L': 'u',
+            'b': 't', 'M': 'v', 'd': 'w', 'F': 'x', 'I': 'y',
+            'A': 'z', 's': '0', '2': '1', '9': '2', 'h': '3',
+            'p': '4', 'x': '5', '0': '6', 'U': '7', 'G': '8',
+            'H': '9', 'i': '-', 'l': '_', 'R': '.', 'z': '.',
+            '8': '.', 'Y': '.', 'v': '.', 'a': '@', 'B': '@',
+            'c': '@', 'D': '@', '5': '@'
+        };
     }
 
     // Generate a random unique value for '.'
